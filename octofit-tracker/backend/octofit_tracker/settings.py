4d2185f2f063ee22cf8169df71a08b4f397e0321
blackie6091 <96027356+blackie6091@users.noreply.github.com>
2025-04-10 20:16:27 +0000

    Adjust settings

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -12,7 +12,9 @@ DATABASES = {
 INSTALLED_APPS += [
     'corsheaders',
 ]
+
 MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = [
     'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'
