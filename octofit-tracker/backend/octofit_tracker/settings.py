a00d36d911bf8dc74b14270bb3725311705d0d8e
blackie6091 <96027356+blackie6091@users.noreply.github.com>
2025-04-10 20:27:45 +0000

    Try another method of adding djongo

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -13,14 +13,33 @@ INSTALLED_APPS += [
     'corsheaders',
 ]
 
-MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # CORS middleware should be placed here
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_METHODS = [
-    'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
 ]
+
 CORS_ALLOW_HEADERS = [
-    'content-type', 'authorization', 'x-csrftoken', 'x-requested-with'
+    'content-type',
+    'authorization',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 # Allow all hosts
